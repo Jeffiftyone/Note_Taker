970040de4bbd6f7f262f9b39f30b5510be905d27
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-10-18 21:24:33 -0400

    updated file structure

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.port || 3001;
+const app = express();
+const api = require('./routes/index.js');
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+
+//GET route for homepage
+app.get('/', (req, res)=>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for notes page
+app.get('/notes', (req,res)=>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+app.listen(PORT, ()=>
+console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
